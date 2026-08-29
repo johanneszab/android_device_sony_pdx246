2ca3a8ba9856e21ec9c92314a155f0bd148d99a9
Johannes Meyer zum Alten Borgloh <code@jzab.de>
2026-08-29 14:18:08 +0200

    Revert "tmp"
    This reverts commit 2944cdde8e7203d2bcaca7f348b5bf52b9df2ea3.

--- extract-files.py
+++ extract-files.py
@@ -23,26 +23,6 @@ namespace_imports = [
     'device/sony/pdx246',
 ]
 
-def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
-    return f'{lib}_{partition}' if partition == 'vendor' else None
-
-lib_fixups: lib_fixups_user_type = {
-    **lib_fixups,
-    (
-        'vendor.qti.hardware.data.connectionaidl-V1-ndk.so',
-        'vendor.qti.diaghal-V1-ndk.so',
-        'graphicbuffersource-aidl-ndk.so',
-        'libaconfig_storage_read_api_cc.so',
-        'libmedia_codeclist.so',
-        'libmedia_codeclist_capabilities.so',
-        'libstagefright_aidl_bufferpool2.so',
-        'libstagefright_codecbase.so',
-        'libstagefright_framecapture_utils.so',
-        'libstagefright_graphicbuffersource_aidl.so',
-        'libstagefright_surface_utils.so',
-    ): lib_fixup_vendor_suffix,
-}
-
 module = ExtractUtilsModule(
     'pdx246',
     'sony',
