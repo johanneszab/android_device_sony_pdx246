7adce3e5738c55c9ce713c0baa70b7af1a8afd90
Johannes Meyer zum Alten Borgloh <code@jzab.de>
2026-08-29 19:03:21 +0200

    add namespaces for blobs

--- extract-files.py
+++ extract-files.py
@@ -21,11 +21,17 @@ from extract_utils.main import (
 
 namespace_imports = [
     'device/sony/pdx246',
+    'hardware/qcom-caf/sm8450-6.6',
+    'hardware/qcom-caf/wlan',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/commonsys/display',
+    'vendor/qcom/opensource/dataservices',
 ]
 
 module = ExtractUtilsModule(
     'pdx246',
     'sony',
+    namespace_imports=namespace_imports,
 )
 
 if __name__ == '__main__':
